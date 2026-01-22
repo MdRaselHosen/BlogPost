e9eab645d15a1b7cb045eaba27e29ae6992c08d4
MdRaselHosen <raselh280@gmail.com>
2026-01-23 02:53:08 +0600

    add Update and Delete functionality

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,7 @@ urlpatterns = [
     path("", views.home, name='home'),
     path("blog_detail/<int:pk>/", views.blog_details, name="blog_detail"),
     path("create/", views.create, name="create"),
+    path("update/<int:pk>/", views.update, name="update"),
+    path("delete/<int:pk>/", views.delete, name="delete"),
+
 ]
--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import *
 from django.contrib.auth.decorators import login_required
@@ -40,3 +40,24 @@ def create(request):
 
 
     return render(request, "create_blog.html")
+
+@login_required
+def update(request, pk):
+    post =get_object_or_404(Post,pk=pk)
+
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        body = request.POST.get('content')
+        post.title = title
+        post.content = body
+        post.save()
+
+        return redirect('blog_detail', pk=post.pk)
+    
+    return render(request, "update.html", {'post':post})
+
+@login_required
+def delete(request, pk):
+    delete_post = Post.objects.get(pk=pk)
+    delete_post.delete()
+    return redirect('home')
